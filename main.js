177b11cdd9804d53c53f23d8a1cfa98bd06c4b13
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-07-02 21:51:32 -0600

    Clear screen with solid black

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	// draw
 	var loop = function() {
-		gl.clearColor(0.75, 0.85, 0.8, 1.0);
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
 		gl.clear(gl.COLOR_BUFFER_BIT);
 
 		gl.useProgram(prog);
