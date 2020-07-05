4be4cd420534fb8d242eea260c2f7df34c26b236
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-07-04 20:46:51 -0600

    Format, refactor

--- shad.fs
+++ shad.fs
@@ -6,17 +6,16 @@ varying vec3
 	_norm;
 
 vec3
-	lightPos = vec3(3, 3, 3),
+	lightPos = vec3(3.0, 3.0, 3.0),
 	lightColor = vec3(1.0),
 	objectColor = vec3(1.0);
 
 void main() {
 	vec3 lightDir = normalize(lightPos - _posFrag);
 
-	float diff = max(dot(_norm, lightDir), 0.0);
-	vec3 diffuse = diff * lightColor;
+	float diff = dot(_norm, lightDir);
 
-	vec3 result = diffuse * objectColor;
+	vec3 result = (diff * lightColor) * objectColor;
 
 	gl_FragColor = vec4(result, 1.0);
 }
