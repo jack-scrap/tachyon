a17863c49cfaef4bce0ac502ba39378f47fe5704
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-07-04 20:50:54 -0600

    Format

--- shad.fs
+++ shad.fs
@@ -7,15 +7,15 @@ varying vec3
 
 vec3
 	lightPos = vec3(3.0, 3.0, 3.0),
-	lightColor = vec3(1.0),
-	objectColor = vec3(1.0);
+	colLight = vec3(1.0),
+	colObj = vec3(1.0);
 
 void main() {
 	vec3 lightDir = normalize(lightPos - _posFrag);
 
 	float diff = max(dot(_norm, lightDir), 0.1);
 
-	vec3 result = (diff * lightColor) * objectColor;
+	vec3 result = (diff * colLight) * colObj;
 
 	gl_FragColor = vec4(result, 1.0);
 }
