fcf1540a3fef77a3dae4d7dde2f4e56e3ae15d44
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-07-04 20:51:16 -0600

    Format

--- shad.fs
+++ shad.fs
@@ -1,19 +1,19 @@
 precision mediump float;
 
+vec3
+	posLight = vec3(3.0, 3.0, 3.0),
+	colLight = vec3(1.0),
+	colObj = vec3(1.0);
+
 varying vec3
 	_pos,
 	_posFrag,
 	_norm;
 
-vec3
-	lightPos = vec3(3.0, 3.0, 3.0),
-	colLight = vec3(1.0),
-	colObj = vec3(1.0);
-
 void main() {
-	vec3 lightDir = normalize(lightPos - _posFrag);
+	vec3 dirLight = normalize(posLight - _posFrag);
 
-	float diff = max(dot(_norm, lightDir), 0.1);
+	float diff = max(dot(_norm, dirLight), 0.1);
 
 	vec3 result = (diff * colLight) * colObj;
 
